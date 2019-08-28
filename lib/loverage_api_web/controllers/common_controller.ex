7d3f14c11e6c9987dde8c6bb5e212ff4930ee0cf
zetton110 <zetton110@gmail.com>
2019-08-29 01:54:01 +0900

    Timexを追加し、日本時刻となるように調整

--- lib/loverage_api_web/controllers/common_controller.ex
+++ lib/loverage_api_web/controllers/common_controller.ex
@@ -31,12 +31,19 @@ defmodule LoverageWeb.CommonController do
   # @hot_topics_limit 3
   # @latest_limit 3
   # @editors_topick_limit 4
-  @others1_topics_limit 4
+  @others1_topics_limit 3
   @others2_topics_limit 4
+  @others3_topics_limit 4
 
   def indexies(conn, common_params) do
+
+    # visualsの取得
+    visuals = Pickup.list_visuals() |> Enum.flat_map(fn(x) -> [x.posts] end)
+
     # featuredの取得
-    top_featureds = Pickup.list_featureds(%{"limit" => 4})
+    dow = NaiveDateTime.utc_now |> Timex.shift(hours: 9) |> Date.day_of_week |> Integer.to_string
+    key_visual = Pickup.list_visuals(%{ "type" => "main", "day_of_week_no" => dow }) |> Enum.at(0)
+    sub_visual = Pickup.list_visuals(%{ "type" => "sub", "day_of_week_no" => dow }) |> Enum.at(0)
 
     # 男性向けFuturedを取得
     %{post_id: id} = Pickup.fetch_big_futured_topic_for("m")
@@ -57,8 +64,7 @@ defmodule LoverageWeb.CommonController do
 
     # ------------- 以降、既に取得済みの記事は除いて取得する -------------------
     # idを集約
-    all_posts =
-      top_featureds ++ [big_futured_for_m] ++ [big_futured_for_f] ++ rankings_view ++ latests
+    all_posts = visuals ++ [big_futured_for_m] ++ [big_futured_for_f] ++ rankings_view ++ latests
 
     # featuredの取得
     featureds_exclude = aggrigate_exclude_ids(all_posts, [0])
@@ -87,12 +93,15 @@ defmodule LoverageWeb.CommonController do
 
     # Topicsの取得
     topics_exclude = aggrigate_exclude_ids(editors_picks, editors_picks_exclude)
-    topics = Discussion.list_posts(%{ "limit" => 40, "exclude" => topics_exclude })
-    others_1 = Enum.slice(topics, 8, @others1_topics_limit)
-    others_2 = Enum.slice(topics, @others1_topics_limit, @others2_topics_limit)
-
+    topics = Discussion.list_posts(%{ "limit" => 30, "exclude" => topics_exclude })
+    others_2 = Enum.slice(topics, 0, @others2_topics_limit)
+    others_3 = Enum.slice(topics, @others2_topics_limit + 1, @others3_topics_limit)
+    others_1 = Enum.slice(topics, @others2_topics_limit + @others3_topics_limit + 1, @others1_topics_limit)
+    
     # 概要の生成
     over_view = %{
+      key_visual: key_visual.posts,
+      sub_visual: sub_visual.posts,
       big_futured_for_m: big_futured_for_m,
       big_futured_for_f: big_futured_for_f,
       rankings_view: rankings_view,
@@ -100,10 +109,10 @@ defmodule LoverageWeb.CommonController do
       latests: latests,
       hot_topics: hot_topics,
       editors_picks: editors_picks,
-      top_featureds: top_featureds,
       featureds: featureds,
       others_1: others_1,
-      others_2: others_2
+      others_2: others_2,
+      others_3: others_3
     }
 
     # JSONの描画
